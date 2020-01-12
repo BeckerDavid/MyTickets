5fff6b3405a43e4de80f54b4ab0c32126dea6b1e
BeckerDavid <46517364+BeckerDavid@users.noreply.github.com>
2020-01-12 23:58:29 +0100

    remove venv

--- myTickets_backend/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Projekte\SWENGS\MyTickets\myTickets_backend\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
